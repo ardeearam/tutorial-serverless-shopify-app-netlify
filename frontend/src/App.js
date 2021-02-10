3bcc89aa556d606e0a444af6e7a99d1dde0c12c6
Ardee Aram <ardee.aram@gmail.com>
2021-02-10 06:21:45 +0000

    Add React via create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import { Heading, Page } from "@shopify/polaris";
+import "@shopify/polaris/dist/styles.css";
+import translations from "@shopify/polaris/locales/en.json";
+
+const App = () => (
+  <Page>
+    <Heading>Shopify app with Node and React 🎉</Heading>
+  </Page>
+);
+
+export default App;
